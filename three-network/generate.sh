c9e68ce5e563e1466a2dfadc4d149d1c86ff8051
saarc <saarc@ajou.ac.kr>
2021-11-17 20:58:03 +0900

    first commit

---
+++ three-network/generate.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+# cryptogen, configtxgen 바이너리를 참조하기 위한 PATH경로 추가
+export PATH=~/fabric-samples/bin:$PATH
+# configtxgen -> configtx.yaml 찾아갈때 사용하는 환경변수
+export FABRIC_CFG_PATH=${PWD}
+
+CHANNEL_NAME=rechannel #-> papernet
+
+# remove previous crypto material and config transactions
+rm -fr config
+mkdir config
+rm -fr crypto-config/*
+
+# generate crypto material
+cryptogen generate --config=./crypto-config.yaml
+if [ "$?" -ne 0 ]; then
+  echo "Failed to generate crypto material..."
+  exit 1 # fatal error
+fi
+
+# generate genesis block for orderer configtx.yaml <- FABRIC_CFG_PATH
+configtxgen -profile ThreeOrgOrdererGenesis -outputBlock ./config/genesis.block
+if [ "$?" -ne 0 ]; then
+  echo "Failed to generate orderer genesis block..."
+  exit 1
+fi
+
+# generate channel configuration transaction
+configtxgen -profile ThreeOrgChannel -outputCreateChannelTx ./config/channel.tx -channelID $CHANNEL_NAME
+if [ "$?" -ne 0 ]; then
+  echo "Failed to generate channel configuration transaction..."
+  exit 1
+fi
+
+# generate anchor peer transaction
+configtxgen -profile ThreeOrgChannel -outputAnchorPeersUpdate ./config/Org1MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org1MSP
+if [ "$?" -ne 0 ]; then
+  echo "Failed to generate anchor peer update for Org1MSP..."
+  exit 1
+fi
+
+configtxgen -profile ThreeOrgChannel -outputAnchorPeersUpdate ./config/Org2MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org2MSP
+if [ "$?" -ne 0 ]; then
+  echo "Failed to generate anchor peer update for Org2MSP..."
+  exit 1
+fi
+
+configtxgen -profile ThreeOrgChannel -outputAnchorPeersUpdate ./config/Org3MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org3MSP
+if [ "$?" -ne 0 ]; then
+  echo "Failed to generate anchor peer update for Org3MSP..."
+  exit 1
+fi
---
+++ three-network/start.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#
+# Copyright IBM Corp All Rights Reserved
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Exit on first error, print all commands.
+set -ev
+
+# don't rewrite paths for Windows Git Bash users
+export MSYS_NO_PATHCONV=1
+
+export CA1KEY=$(cd crypto-config/peerOrganizations/org1.reshop.com/ca/ && ls *_sk)
+export CA2KEY=$(cd crypto-config/peerOrganizations/org2.reshop.com/ca/ && ls *_sk)
+export CA3KEY=$(cd crypto-config/peerOrganizations/org3.reshop.com/ca/ && ls *_sk)
+
+docker-compose -f docker-compose.yml down
+
+# docker-compose -> 컨테이터수행 및 net_basic 네트워크 생성
+docker-compose -f docker-compose.yml up -d ca.org1.reshop.com ca.org2.reshop.com ca.org3.reshop.com orderer.reshop.com peer0.org1.reshop.com  peer0.org2.reshop.com peer0.org3.reshop.com cli
+
+docker ps -a
+docker network ls
+
+# wait for Hyperledger Fabric to start
+# incase of errors when running later commands, issue export FABRIC_START_TIMEOUT=<larger number>
+export FABRIC_START_TIMEOUT=5
+#echo ${FABRIC_START_TIMEOUT}
+sleep ${FABRIC_START_TIMEOUT}
+
+# Create the channel -> rechannel.block cli working dir 복사
+docker exec cli peer channel create -o orderer.reshop.com:7050 -c rechannel -f /etc/hyperledger/configtx/channel.tx
+# clie workding dir (/etc/hyperledger/configtx/) rechannel.block
+
+sleep 3
+
+# Join peer0.org1.reshop.com to the channel.
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.reshop.com/msp" peer0.org1.reshop.com peer channel join -b /etc/hyperledger/configtx/rechannel.block
+
+sleep 3
+
+# Join peer0.org2.reshop.com to the channel.
+docker exec -e "CORE_PEER_LOCALMSPID=Org2MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org2.reshop.com/msp" peer0.org2.reshop.com peer channel join -b /etc/hyperledger/configtx/rechannel.block
+
+sleep 3
+
+# anchor ORG1 rechannel update
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.reshop.com/msp" peer0.org1.reshop.com peer channel update -f /etc/hyperledger/configtx/Org1MSPanchors.tx -c rechannel -o orderer.reshop.com:7050
+
+sleep 3
+
+# anchor ORG2 rechannel update
+docker exec -e "CORE_PEER_LOCALMSPID=Org2MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org2.reshop.com/msp" peer0.org2.reshop.com peer channel update -f /etc/hyperledger/configtx/Org2MSPanchors.tx -c rechannel -o orderer.reshop.com:7050
+
+sleep 3
